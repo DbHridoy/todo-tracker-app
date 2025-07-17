d999ef4af494e0b46bbbf77f95d1f315bc9a7a8c
DbHridoy <mdnuralam378@gmail.com>
2025-07-17 22:52:28 +0600

    error solved

--- backendp/src/db/db.js
+++ backendp/src/db/db.js
@@ -3,23 +3,23 @@ import mongoose from 'mongoose';
 import { DB_NAME } from '../contants.js';
 import asyncHandler from '../utils/AsyncHandler.js';
 
-// const connectDB = async () => {
-//   try {
-//     const connectionInstance = await mongoose.connect(
-//       `${process.env.MONGO_URI}/${DB_NAME} `
-//     );
-//     console.log(
-//       'MongoDB connected to host',
-//       connectionInstance.connection.host
-//     );
-//   } catch (error) {
-//     // console.log(10000, process.env.MONGO_URI);
-//     console.error('MongoDB connection error:', error);
-//     throw error;
-//   }
-// };
-const connect = () => mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGO_URI}/${DB_NAME} `
+    );
+    console.log(
+      'MongoDB connected to host',
+      connectionInstance.connection.host
+    );
+  } catch (error) {
+    // console.log(10000, process.env.MONGO_URI);
+    console.error('MongoDB connection error:', error);
+    throw error;
+  }
+};
+// const connect = () => mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
 
-const connectDB = asyncHandler(connect);
+// const connectDB = asyncHandler(connect);
 
 export default connectDB;
