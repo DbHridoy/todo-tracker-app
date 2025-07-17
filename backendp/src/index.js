8bbb3b4fb222a04cc2fdd41a0ebcea1f7858d366
DbHridoy <mdnuralam378@gmail.com>
2025-07-17 22:52:28 +0600

    error solved

--- backendp/src/index.js
+++ backendp/src/index.js
@@ -1,17 +1,17 @@
 import dotenv from 'dotenv/config';
 import connectDB from './db/db.js';
+import mongoose from 'mongoose';
 import app from './app.js';
 
-const cd = connectDB;
-app.listen(process.env.PORT || 5000, () => {
-  console.log(`Server is running on port ${process.env.PORT || 5000}`);
-});
-//   .then(() => {
-//    );
-//     app.get('/', (req, res) => {
-//       res.send('Welcome to the Todo Tracker App Backend!');
-//     });
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//   });
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on port ${process.env.PORT || 3000}`);
+    });
+  })
+  .then(() => {
+    app.get('/', (req, res) => {
+      res.send('Welcome to the Todo API');
+    });
+  })
+  .catch((err) => {});
